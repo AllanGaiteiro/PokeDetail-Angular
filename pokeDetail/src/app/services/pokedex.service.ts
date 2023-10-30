d11e0662b0d6929304e8a9941871a5d90c08a7e7
G4iteir00 <149211758+G4iteir00@users.noreply.github.com>
2023-10-30 03:10:50 +0000

    fix: service pokedex response observable in observable

--- pokeDetail/src/app/services/pokedex.service.ts
+++ pokeDetail/src/app/services/pokedex.service.ts
@@ -1,28 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
-import { PokemonList } from '../models/pokemon-list.model';
+import { Observable, forkJoin } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
+import { PokemonData } from '../models/pokemon-data.model';
 import { Pokemon } from '../models/pokemon.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokedexService {
   private baseUrl = 'https://pokeapi.co/api/v2/';
 
   constructor(private http: HttpClient) {}
 
-  getPokemonList(offset: number = 0, limit: number = 20): Observable<PokemonList> {
+
+  getPokemonList(offset: number = 0, limit: number = 20): Observable<any> {
+    console.log(offset, offset + limit);
     const url = `${this.baseUrl}pokemon/?offset=${offset}&limit=${limit}`;
     return this.http.get(url).pipe(
-      map((data: any) => new PokemonList(data))
+      mergeMap((data: any) => {
+        const pokemonUrls = data.results.map((result: any) => result.url);
+        return forkJoin(
+          pokemonUrls.map((url: string) => this.getPokemonDetails(url))
+        );
+      })
     );
   }
 
-  getPokemonDetails(id: number): Observable<Pokemon> {
-    const url = `${this.baseUrl}pokemon/${id}/`;
-    return this.http.get(url).pipe(
-      map((data: any) => new Pokemon(data))
-    );
+  getPokemonDetails(url: string): Observable<Pokemon> {
+    return this.http
+      .get<PokemonData>(url)
+      .pipe(map((data: PokemonData) => new Pokemon(data)));
   }
 }
