8dd135117864bf435fcead457943f4e9d28c4f99
G4iteir00 <149211758+G4iteir00@users.noreply.github.com>
2023-10-30 03:13:12 +0000

    feat: add component pokemon card and create scroll of card

--- pokeDetail/src/app/pages/page-list/page-list.component.ts
+++ pokeDetail/src/app/pages/page-list/page-list.component.ts
@@ -1,21 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
+
 import { PokedexService } from '../../services/pokedex.service';
+import { Pokemon } from 'src/app/models/pokemon.model';
 
 @Component({
   selector: 'app-page-list',
   templateUrl: './page-list.component.html',
-  styleUrls: ['./page-list.component.css']
+  styleUrls: ['./page-list.component.css'],
 })
 export class PageListComponent implements OnInit {
-
-  constructor(private pokemonService: PokedexService){
-
-  }
-  ngOnInit(): void {
-    this.pokemonService.getPokemonList().subscribe((data) => {
-      console.log(data);
-    });
+  pokemonList: Pokemon[] = [];
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    this.checkScroll();
   }
 
-  
+  totalPokemons: number = 0;
+
+  constructor(private pokedexService: PokedexService, private el: ElementRef) {}
+
+  ngOnInit() {
+    this.loadPokemonList();
+  }
+
+  loadPokemonList() {
+    this.pokedexService.getPokemonList(this.totalPokemons).subscribe(
+      (data: Pokemon[]) => {
+        this.pokemonList.push(...data);
+        this.totalPokemons += this.pokemonList.length;
+      },
+      (error) => {
+        console.error('Erro ao carregar a lista de Pokémon', error);
+      }
+    );
+  }
+
+  checkScroll() {
+    const element = this.el.nativeElement;
+    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+      this.loadPokemonList();
+    }
+  }
 }
