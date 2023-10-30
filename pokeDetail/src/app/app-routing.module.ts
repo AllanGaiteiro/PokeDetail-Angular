97c5ccbd05b0f1ec45c50f2d3635f14d7797c41b
G4iteir00 <149211758+G4iteir00@users.noreply.github.com>
2023-10-30 03:11:17 +0000

    feat: change router

--- pokeDetail/src/app/app-routing.module.ts
+++ pokeDetail/src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageListComponent } from './pages/page-list/page-list.component';
 import { PageViewComponent } from './pages/page-view/page-view.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [
-  {path:'lista', component: PageListComponent},
-  {path:'pokemon/:id' ,component: PageViewComponent},
+  { path: '', redirectTo: '/lista', pathMatch: 'full' },
+  { path: 'lista', component: PageListComponent },
+  { path: 'pokemon/:id', component: PageViewComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), BrowserModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
