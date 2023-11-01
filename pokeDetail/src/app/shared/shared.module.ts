6c9cf4aa133581904d143c3640dd47dc7a19b737
G4iteir00 <149211758+G4iteir00@users.noreply.github.com>
2023-11-01 23:34:18 +0000

    feat: add news components in shared module and page view

--- pokeDetail/src/app/shared/shared.module.ts
+++ pokeDetail/src/app/shared/shared.module.ts
@@ -6,10 +6,29 @@ import { PokemonTypeComponent } from './pokemon-type/pokemon-type.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+import { PokemonImageComponent } from './pokemon-image/pokemon-image.component';
+import { StatBarComponent } from './stat-bar/stat-bar.component';
+import { PokemonStatsComponent } from './pokemon-stats/pokemon-stats.component';
 
 @NgModule({
-  declarations: [PokemonCardComponent, PokemonTypeComponent, FilterComponent],
+  declarations: [
+    PokemonCardComponent,
+    PokemonTypeComponent,
+    FilterComponent,
+    PokemonDetailsComponent,
+    PokemonImageComponent,
+    StatBarComponent,
+    PokemonStatsComponent,
+  ],
   imports: [BrowserModule, HttpClientModule, FormsModule],
-  exports: [PokemonCardComponent, PokemonTypeComponent, FilterComponent],
+  exports: [
+    PokemonCardComponent,
+    PokemonTypeComponent,
+    FilterComponent,
+    PokemonDetailsComponent,
+    PokemonImageComponent,
+    PokemonStatsComponent
+  ],
 })
 export class SharedModule {}
